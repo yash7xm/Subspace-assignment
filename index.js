9faf3a36904e96155a076aeac02a3b07e2556701
yash7xm <yash.7xm@gmail.com>
2023-09-30 18:40:07 +0530

    analyticsResult done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const _ = require('lodash');
 const app = express();
 const port = 8080;
 
@@ -11,8 +12,24 @@ const headers = {
 
 app.get('/api/blog-stats', async (req, res) => {
     const response = await axios.get(blogURL, { headers });
-   
-    res.send(response.data);
+    const blogData = response.data.blogs;
+
+    const totalBlogs = blogData.length;
+    const blogWithLongestTitle = _.maxBy(blogData, 'title.length');
+    const blogsWithPrivacyInTitle = _.filter(blogData, (blog) =>
+        _.includes(_.toLower(blog.title), 'privacy')
+    );
+    const numberOfBlogsWithPrivacyTitle = blogsWithPrivacyInTitle.length;
+    const uniqueBlogTitles = _.uniqBy(blogData, 'title').map((blog) => blog.title);
+    
+    const analyticsResult = {
+        totalBlogs,
+        blogWithLongestTitle,
+        numberOfBlogsWithPrivacyTitle,
+        uniqueBlogTitles
+    }
+
+    res.send(analyticsResult);
 })
 
 
