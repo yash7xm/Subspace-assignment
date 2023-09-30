3d712fce45a21e2a166373a7d9b41858c2454320
yash7xm <yash.7xm@gmail.com>
2023-09-30 19:28:02 +0530

    memoized data (bonus challenge)

--- index.js
+++ index.js
@@ -9,10 +9,54 @@ const headers = {
     'x-hasura-admin-secret': '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6'
 };
 
+const cachedDuration = 10*60*1000;
+const memoizedStats = _.memoize(calculateStats, undefined, cachedDuration);
+const memoizedSearch = _.memoize(searchBlogs, undefined, cachedDuration);
 
 app.get('/api/blog-stats', async (req, res) => {
+
+    try {
+
+        const analyticsResult = await memoizedStats();
+
+        res.send(analyticsResult);
+
+    }
+    catch (error) {
+        console.error(`Error fetching blog data: ${error.message}`);
+        res.status(500).json({ error: 'Failed to fetch blog stats' });
+    }
+})
+
+app.get('/api/blog-search', async (req, res) => {
+   try {
+    const query = req.query.query;
+
+    if(!query){
+        res.status(400).json({error: "Query parameter is missing"})
+        return;
+    }
+
+  
+    const searchResults = await memoizedSearch(query);
+    
+
+    res.send(searchResults);
+   }
+   catch (error){
+    console.error(`Error processing blog search: ${error.message}`);
+    res.status(500).json({ error: 'Failed to process blog search' });
+   }
+})
+
+async function fetchBlogData() {
     const response = await axios.get(blogURL, { headers });
     const blogData = response.data.blogs;
+    return blogData;
+}
+
+async function calculateStats() {
+    const blogData = await fetchBlogData();
 
     const totalBlogs = blogData.length;
     const blogWithLongestTitle = _.maxBy(blogData, 'title.length');
@@ -29,26 +73,18 @@ app.get('/api/blog-stats', async (req, res) => {
         uniqueBlogTitles
     }
 
-    res.send(analyticsResult);
-})
+    return analyticsResult;
+}
 
-app.get('/api/blog-search', async (req, res) => {
-    const query = req.query.query;
-
-    if(!query){
-        res.status(400).json({error: "Query parameter is missing"})
-        return;
-    }
-
-    const response = await axios.get(blogURL, { headers });
-    const blogData = response.data.blogs;
+async function searchBlogs(query) {
+    const blogData = await fetchBlogData();
 
     const searchResults = _.filter(blogData, (blog) => 
      _.includes(blog.title, query)
     );
-
-    res.send(searchResults);
-})
+    
+    return searchResults;
+}
 
 
 
